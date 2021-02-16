f638e81b0894dc64bac488ee78f061192932d94f
Arseni-Lukyanau <61196838+Arseni-Lukyanau@users.noreply.github.com>
2021-02-16 11:40:43 +0300

    Add files via upload

---
+++ смаил.pas
@@ -0,0 +1,12 @@
+﻿uses  graphABC;
+begin
+  SetWindowSize(300, 300);
+  Circle(150, 150, 100);
+  FloodFill(150, 150, clYellow);
+  Ellipse(110, 90, 130, 150);
+  FloodFill(120, 115, clBlack);
+  Ellipse(170, 90, 190, 150);
+  FloodFill(180, 120, clBlack);
+  SetPenWidth(3);
+  Arc(150, 170, 50, 0, -180);
+end.
